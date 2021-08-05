8d6ef8da800613644491f7a077624969c03b4485
Eric Hale <eric_hale@comcast.net>
2021-08-05 00:29:28 -0600

    one line solution

--- lib/temp.rb
+++ lib/temp.rb
@@ -1,9 +1,5 @@
 class Temp
     def apple(x)
-        if x.to_i ** 2 > 1000
-            "It's hotter than the sun!!"
-        else
-            "Help yourself to a honeycomb Yorkie for the glovebox."
-        end
+        x.to_i ** 2 >= 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
     end
 end
